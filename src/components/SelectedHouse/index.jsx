d738f37584f659e5bc5f22087130c2826866d8d4
mirjahon <mirjahonoralov@gmail.com>
2022-07-18 19:46:03 +0500

    product

--- src/components/SelectedHouse/index.jsx
+++ src/components/SelectedHouse/index.jsx
@@ -2,7 +2,16 @@ import React, { useState } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { useHttp } from "../../hooks/useHttp";
-import { Container } from "./style";
+import {
+  Actions,
+  Container,
+  Content,
+  Description,
+  Icons,
+  Params,
+  Price,
+  Wrapper,
+} from "./style";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
 const SelectedHouse = () => {
@@ -43,11 +52,96 @@ const SelectedHouse = () => {
   });
   return (
     <Container>
-      {isLoaded && (
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-          {center.lat && center.lng && <Marker position={center} />}
-        </GoogleMap>
-      )}
+      <Wrapper>
+        <Content>
+          <h1>
+            Luxury Family Loft by Victoria Park
+            <Actions>
+              <span>
+                <div className="description">Share</div>
+                <div className="icon-wrapper">
+                  <Icons.Share />
+                </div>
+              </span>
+              <span>
+                <div className="description">Like</div>
+                <div className="icon-wrapper">
+                  <Icons.Like />
+                </div>
+              </span>
+            </Actions>
+          </h1>
+          <h1>
+            <div>
+              <div className="description">Quincy St, Brooklyn, NY, USA</div>
+              <Params>
+                <span>
+                  <Icons.Bed />
+                  <div className="description">4 beds</div>
+                </span>
+                <span>
+                  <Icons.Bath />
+                  <div className="description">4 baths</div>
+                </span>
+                <span>
+                  <Icons.Garage />
+                  <div className="description">4 garages</div>
+                </span>
+                <span>
+                  <Icons.Ruler />
+                  <div className="description">1200 Sq Ft</div>
+                </span>
+                <span>
+                  <Icons.Calendar />
+                  <div className="description">Year Built: 1800</div>
+                </span>
+              </Params>
+            </div>
+            <Price>
+              <div>
+                <span className="delete description">$2,800/mo</span>
+                <span className="title">$7,500/mo</span>
+              </div>
+              <div className="description">Est. Mortgage</div>
+            </Price>
+          </h1>
+
+          <Description>
+            <div className="title">Description</div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, dicta
+              at. Ad voluptatum corrupti ullam in debitis excepturi explicabo
+              quam ratione dolorem molestias eos quasi veniam recusandae error
+              qui doloremque magnam rem vitae id laudantium, dolores nostrum?
+              Neque asperiores, iste dolore repudiandae similique earum adipisci
+              pariatur incidunt hic quas, ab laborum nemo tempore? Temporibus
+              molestias quia corrupti mollitia cumque, eveniet eius omnis, amet
+              commodi enim nihil laudantium impedit ipsum magni.
+              <br />
+              <br />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum
+              asperiores, odio a minima aut molestias impedit at distinctio
+              incidunt error corporis maxime dolorum nostrum neque possimus
+              assumenda, nulla veniam maiores necessitatibus aspernatur
+              repudiandae, natus delectus voluptate? Pariatur odio blanditiis
+              natus impedit maxime illo corrupti quibusdam assumenda? Facere
+              enim asperiores amet?{" "}
+            </p>
+
+            <a href="#">Show more</a>
+          </Description>
+
+          {isLoaded && (
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={10}
+            >
+              {center.lat && center.lng && <Marker position={center} />}
+            </GoogleMap>
+          )}
+        </Content>
+      </Wrapper>
     </Container>
   );
 };
--- src/components/SelectedHouse/style.js
+++ src/components/SelectedHouse/style.js
@@ -1,7 +1,110 @@
 import styled from "styled-components";
+import { ReactComponent as share } from "../../assets/icons/properties/share.svg";
+import { ReactComponent as like } from "../../assets/icons/properties/like.svg";
+import { ReactComponent as calendar } from "../../assets/icons/properties/calendar.svg";
+import { ReactComponent as bed } from "../../assets/icons/properties/bed.svg";
+import { ReactComponent as bath } from "../../assets/icons/properties/bath.svg";
+import { ReactComponent as ruler } from "../../assets/icons/properties/ruler.svg";
+import { ReactComponent as garage } from "../../assets/icons/properties/car.svg";
 
 const Container = styled.div`
   display: flex;
+  padding: 130px;
+  /* max-width: 1440px; */
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `;
 
-export { Container };
+const Wrapper = styled.div`
+  display: flex;
+  width: 1440px;
+`;
+
+const Content = styled.div`
+  width: 80%;
+
+  h1 {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Actions = styled.div`
+  display: flex;
+  gap: 25px;
+  span {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    .icon-wrapper {
+      background: #f6f8f9;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 10px;
+      border-radius: 100%;
+      cursor: pointer;
+    }
+  }
+`;
+
+const Params = styled.div`
+  margin-top: 36px;
+  display: flex;
+  gap: 25px;
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+`;
+
+const Price = styled.div`
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 4px;
+
+  div:nth-child(1) {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+`;
+
+const Description = styled.div`
+  margin-top: 48px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  p {
+    margin-top: 16px;
+  }
+`;
+
+const Icons = styled.div``;
+
+Icons.Share = share;
+Icons.Like = like;
+Icons.Calendar = calendar;
+Icons.Bed = bed;
+Icons.Bath = bath;
+Icons.Ruler = ruler;
+Icons.Garage = garage;
+
+export {
+  Description,
+  Container,
+  Wrapper,
+  Content,
+  Actions,
+  Icons,
+  Params,
+  Price,
+};
